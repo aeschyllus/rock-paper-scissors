b66fbbe6ddedc5ab9b2708426a7d836371d85c2d
aeschyllus <lapadamarkanthony@gmail.com>
2024-07-14 10:48:55 +0800

    chore: import assets

---
+++ src/constants/images.ts
@@ -0,0 +1,17 @@
+import BgTriangle from "../assets/bg-triangle.svg";
+import IconClose from "../assets/icon-close.svg";
+import IconPaper from "../assets/icon-paper.svg";
+import IconRock from "../assets/icon-rock.svg";
+import IconScissors from "../assets/icon-scissors.svg";
+import Logo from "../assets/logo.svg";
+import Rules from "../assets/image-rules.svg";
+
+export const SVG = {
+  BG_TRIANGLE: BgTriangle,
+  CLOSE: IconClose,
+  PAPER: IconPaper,
+  ROCK: IconRock,
+  SCISSORS: IconScissors,
+  LOGO: Logo,
+  RULES: Rules,
+};
