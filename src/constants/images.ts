1e3266337c8c428b70d1fb514beac99a03ad474a
aeschyllus <lapadamarkanthony@gmail.com>
2024-07-21 21:46:02 +0800

    chore: update key type of SVG constant

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -6,12 +6,22 @@ import IconScissors from "../assets/icon-scissors.svg";
 import Logo from "../assets/logo.svg";
 import Rules from "../assets/image-rules.svg";
 
-export const SVG: { [key: string]: string } = {
-  BG_TRIANGLE: BgTriangle,
-  CLOSE: IconClose,
-  PAPER: IconPaper,
-  ROCK: IconRock,
-  SCISSORS: IconScissors,
-  LOGO: Logo,
-  RULES: Rules,
+enum SvgKeys {
+  BG_TRIANGLE = "BG_TRIANGLE",
+  CLOSE = "CLOSE",
+  PAPER = "PAPER",
+  ROCK = "ROCK",
+  SCISSORS = "SCISSORS",
+  LOGO = "LOGO",
+  RULES = "RULES",
+}
+
+export const SVG: { [key in SvgKeys]: string } = {
+  [SvgKeys.BG_TRIANGLE]: BgTriangle,
+  [SvgKeys.CLOSE]: IconClose,
+  [SvgKeys.PAPER]: IconPaper,
+  [SvgKeys.ROCK]: IconRock,
+  [SvgKeys.SCISSORS]: IconScissors,
+  [SvgKeys.LOGO]: Logo,
+  [SvgKeys.RULES]: Rules,
 };
