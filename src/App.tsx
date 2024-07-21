83556ee8ebe959e964ce871c34637125e7baab05
aeschyllus <lapadamarkanthony@gmail.com>
2024-07-21 21:49:22 +0800

    chore: adjust position of RPS buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,30 @@ import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <RpsButton type="ROCK" onClick={() => console.log("hi")} />
-      <RpsButton type="PAPER" onClick={() => console.log("hi")} />
-      <RpsButton type="SCISSORS" onClick={() => console.log("hi")} />
-    </div>
+    <main className="app">
+      <div className="rps">
+        <img
+          className="rps__triangle"
+          src={SVG.BG_TRIANGLE}
+          alt="background triangle"
+        />
+        <RpsButton
+          className="rps__button rps__button--rock"
+          type="ROCK"
+          onClick={() => console.log("hi")}
+        />
+        <RpsButton
+          className="rps__button rps__button--paper"
+          type="PAPER"
+          onClick={() => console.log("hi")}
+        />
+        <RpsButton
+          className="rps__button rps__button--scissors"
+          type="SCISSORS"
+          onClick={() => console.log("hi")}
+        />
+      </div>
+    </main>
   );
 }
 
