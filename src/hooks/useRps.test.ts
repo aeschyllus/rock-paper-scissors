e53c8160b8898db34e593d67a12fcc11e87a424c
aeschyllus <lapadamarkanthony@gmail.com>
2024-09-14 12:41:43 +0800

    chore: create custom hook to handle RPS logic

---
+++ src/hooks/useRps.test.ts
@@ -0,0 +1,110 @@
+import { act, renderHook } from "@testing-library/react";
+import { useRps } from "./useRps";
+
+it("should render initial values", () => {
+  const { result } = renderHook(useRps);
+  expect(result.current.score).toBe(0);
+  expect(result.current.userPick).toBe("");
+});
+
+it("should randomly generate a house pick", () => {
+  const { result } = renderHook(useRps);
+  const housePick = result.current.getHousePick();
+  expect(housePick).toMatch(/ROCK|SCISSORS|PAPER/);
+});
+
+describe("getResult", () => {
+  test("tie", () => {
+    const { result } = renderHook(useRps);
+    const { getResult } = result.current;
+    let output1, output2, output3;
+
+    act(() => {
+      output1 = getResult("ROCK", "ROCK");
+      output2 = getResult("PAPER", "PAPER");
+      output3 = getResult("SCISSORS", "SCISSORS");
+    });
+
+    expect(output1).toBe(2);
+    expect(output2).toBe(2);
+    expect(output3).toBe(2);
+  });
+
+  describe("rock", () => {
+    it("should beat scissors", async () => {
+      const { result } = renderHook(useRps);
+      const { getResult } = result.current;
+      let output;
+
+      act(() => {
+        output = getResult("ROCK", "SCISSORS");
+      });
+
+      expect(output).toBe(1);
+    });
+
+    it("should lose to paper", () => {
+      const { result } = renderHook(useRps);
+      const { getResult } = result.current;
+      let output;
+
+      act(() => {
+        output = getResult("ROCK", "PAPER");
+      });
+
+      expect(output).toBe(0);
+    });
+  });
+
+  describe("paper", () => {
+    it("should beat rock", () => {
+      const { result } = renderHook(useRps);
+      const { getResult } = result.current;
+      let output;
+
+      act(() => {
+        output = getResult("PAPER", "ROCK");
+      });
+
+      expect(output).toBe(1);
+    });
+
+    it("should lose to scissors", () => {
+      const { result } = renderHook(useRps);
+      const { getResult } = result.current;
+      let output;
+
+      act(() => {
+        output = getResult("PAPER", "SCISSORS");
+      });
+
+      expect(output).toBe(0);
+    });
+  });
+
+  describe("scissors", () => {
+    it("should beat paper", () => {
+      const { result } = renderHook(useRps);
+      const { getResult } = result.current;
+      let output;
+
+      act(() => {
+        output = getResult("SCISSORS", "PAPER");
+      });
+
+      expect(output).toBe(1);
+    });
+
+    it("should lose to rock", () => {
+      const { result } = renderHook(useRps);
+      const { getResult } = result.current;
+      let output;
+
+      act(() => {
+        output = getResult("SCISSORS", "ROCK");
+      });
+
+      expect(output).toBe(0);
+    });
+  });
+});
---
+++ src/hooks/useRps.ts
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const RPS = ["ROCK", "PAPER", "SCISSORS"];
+
+export function useRps() {
+  const [score, setScore] = useState(0);
+  const [userPick, setUserPick] = useState("");
+
+  /**
+   * @return "ROCK" | "PAPER" | "SCISSORS"
+   */
+  const getHousePick = () => {
+    const index = Math.floor(Math.random() * 3);
+    return RPS[index];
+  };
+
+  /**
+   * Get the result of the epic battle between the user and the house!
+   *
+   * @param user The user's awesome pick
+   * @param house The house's equally awesome pick
+   * @return
+   *   -1 = something went wrong
+   *    0 = lose
+   *    1 = win (hell yeah)
+   *    2 = tie
+   */
+  const getResult = (user: string, house: string) => {
+    if (user === house) {
+      return 2; // tie
+    }
+
+    if (user === "ROCK") {
+      if (house === "PAPER") {
+        setScore((prev) => prev - 1);
+        return 0;
+      } else if (house === "SCISSORS") {
+        setScore((prev) => prev + 1);
+        return 1;
+      }
+    } else if (user === "PAPER") {
+      if (house === "SCISSORS") {
+        setScore((prev) => prev - 1);
+        return 0;
+      } else if (house === "ROCK") {
+        setScore((prev) => prev + 1);
+        return 1;
+      }
+    } else if (user === "SCISSORS") {
+      if (house === "ROCK") {
+        setScore((prev) => prev - 1);
+        return 0;
+      } else if (house === "PAPER") {
+        setScore((prev) => prev + 1);
+        return 1;
+      }
+    }
+
+    return -1;
+  };
+
+  return { score, userPick, getHousePick, getResult, setUserPick };
+}
