fc29cfefd0969bdc5bc7ce1304da340a10625341
aeschyllus <lapadamarkanthony@gmail.com>
2024-09-20 20:58:16 +0800

    chore: make score dynamic

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 
 it("should render", () => {
-  render(<Header />);
+  render(<Header score={69} />);
   expect(screen.getByTestId("header")).toBeInTheDocument();
+  expect(screen.getByText("69")).toBeInTheDocument();
 });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
 import { SVG } from "../../constants/images";
 import "./Header.scss";
 
-export default function Header() {
+type HeaderProps = {
+  score: number;
+};
+
+export default function Header({ score }: HeaderProps) {
   return (
     <header className="header" data-testid="header">
       <img className="header__logo" src={SVG.LOGO} alt="Logo" />
       <div className="header__score">
         <p className="header__score__title">score</p>
-        <p className="header__score__count">12</p>
+        <p className="header__score__count">{score}</p>
       </div>
     </header>
   );
